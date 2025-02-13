9b74c89877ae118afe6a8761dd5a2efcc7fa46fe
Antonio Rueda-Toicen <antonio.rueda.toicen@gmail.com>
2025-02-13 13:32:47 +0000

    compute embeddings

--- 0_1_compute_embeddings.py
+++ 0_1_compute_embeddings.py
@@ -8,8 +8,12 @@ dataset = foz.load_zoo_dataset("quickstart")
 fob.compute_visualization(dataset, brain_key="img_viz")
 
 # Object patch embeddings
+# We use small a batch_size and num_workers=0 to avoid running out of memory
 fob.compute_visualization(
-    dataset, patches_field="ground_truth", brain_key="gt_viz"
+    dataset, patches_field="ground_truth",
+      brain_key="gt_viz", 
+      batch_size=2,
+      num_workers=0
 )
 
 session = fo.launch_app(dataset)
